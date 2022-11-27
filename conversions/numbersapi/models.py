7a6858211bf0a4494eef252bc5494ef39c7be90a
Thiago Krempser <tkrempser@gmail.com>
2022-11-27 13:00:23 -0300

    Add conversion model

--- conversions/numbersapi/models.py
+++ conversions/numbersapi/models.py
@@ -2,11 +2,10 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Order(models.Model):
+class Conversion(models.Model):
     input_number = models.IntegerField()
-    output_words = models.CharField(max_length=200)
-    created_by = models.ForeignKey(
-        User, on_delete=models.CASCADE)
+    output_words = models.CharField(max_length=200, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
