d0e8b8b748f0e803a8afe63e881d8d12acbf70cc
Thiago Krempser <tkrempser@gmail.com>
2022-11-27 11:54:19 -0300

    Add django project

---
+++ conversions/numbersapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NumbersapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'numbersapi'
---
+++ conversions/numbersapi/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Order(models.Model):
+    input_number = models.IntegerField()
+    output_words = models.CharField(max_length=200)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.input_number} - {self.output_words}"
