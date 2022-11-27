b4695b87a1f911dafcfbea7e7841171b015837a1
Thiago Krempser <tkrempser@gmail.com>
2022-11-27 13:27:32 -0300

    Add user + converstion views

--- conversions/numbersapi/views.py
+++ conversions/numbersapi/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.utils import timezone
+from numbersapi.models import Conversion
+from numbersapi.serializers import ConversionSerializer, UserSerializer
+from numbersapi.utils import convert_number_to_words
+from rest_framework import viewsets
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ConversionViewSet(viewsets.ModelViewSet):
+    queryset = Conversion.objects.all()
+    serializer_class = ConversionSerializer
+
+    def perform_create(self, serializer):
+        input_number = serializer.validated_data["input_number"]
+        output_words = convert_number_to_words(input_number)
+
+        serializer.save(output_words=output_words)
