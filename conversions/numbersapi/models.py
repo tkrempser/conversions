6c3446828738d42f17fa07a387c873af7298038b
Thiago Krempser <tkrempser@gmail.com>
2022-11-28 12:06:12 -0300

    Update conversion model + Add migration file

--- conversions/numbersapi/models.py
+++ conversions/numbersapi/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Conversion(models.Model):
     input_number = models.BigIntegerField()
     output_words = models.CharField(max_length=200, blank=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
