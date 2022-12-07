8c448b26ac79b15165d260812ec0f27d0dffd04c
Thiago Krempser <tkrempser@gmail.com>
2022-12-07 10:05:37 -0300

    Update conversion serializer

--- conversions/numbersapi/serializers.py
+++ conversions/numbersapi/serializers.py
@@ -14,4 +14,11 @@ class ConversionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Conversion
         fields = ["url", "input_number", "output_words", "created_by", "created_at"]
-        read_only_fields = ['output_words',]
+        read_only_fields = [
+            "output_words",
+        ]
+
+    def to_representation(self, instance):
+        new_representation = super().to_representation(instance)
+        new_representation["input_number"] = str(new_representation["input_number"])
+        return new_representation
