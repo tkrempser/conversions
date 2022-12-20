cb4ca2332aab5709b22406870c6a8d55eab833c3
Thiago Krempser <tkrempser@gmail.com>
2022-12-20 15:02:55 -0300

    Fix ordering

--- conversions/numbersapi/models.py
+++ conversions/numbersapi/models.py
@@ -9,7 +9,7 @@ class Conversion(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["-created_by"]
+        ordering = ["created_at"]
 
     def __str__(self):
         return f"{self.input_number} - {self.output_words}"
